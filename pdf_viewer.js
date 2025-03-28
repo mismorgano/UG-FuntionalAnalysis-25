89608a693a83994e3e6c5bb8b2697cb4d2e60ba3
mismorgano <mismorgano@gmail.com>
2025-03-27 18:38:46 -0600

    add: Simple pdf viewer using PDFjs I try to maybe use the the pdf_viewer of the project but that implies I need to download viewer.html and maybe include it with embed tag.
    Anyways I didn't find too much documentation about how to use it.
    
    So I'll implement a simple pdf viewer.
    
    TODO:
    - Implement simple pdf viewer.
    - If possible implement scroll.

---
+++ pdf_viewer.js
@@ -0,0 +1,39 @@
+
+
+let url = './Tarea-01.pdf'
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.0.375/pdf.worker.min.mjs'
+
+let loadingTask = pdfjsLib.getDocument(url);
+
+loadingTask.promise.then(pdf => {
+    console.log('PDF loaded');
+
+    let pageNumber = 1;
+    pdf.getPage(pageNumber).then(page => {
+        console.log('Page loaded');
+
+        let scale = 1.5;
+        let viewport = page.getViewport({ scale: scale });
+
+        let canvas = document.getElementById('the-pdf');
+        let context = canvas.getContext('2d');
+
+        canvas.width = viewport.width * window.devicePixelRatio;
+        canvas.height = viewport.height * window.devicePixelRatio;
+        canvas.style.width = viewport.width + "px";
+        canvas.style.height = viewport.height + "px";
+
+        context.scale(window.devicePixelRatio, window.devicePixelRatio);
+        let renderContext = {
+            canvasContext: context,
+            viewport: viewport,
+            intent: 'display'
+        };
+
+        let renderTask = page.render(renderContext);
+        renderTask.promise.then(() => {
+            console.log('Page rendered');
+        })
+    })
+})
